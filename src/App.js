9c7a35af501b78f065045d083d800d9f6978a787
Dan Lopez <danlopez@Dans-MacBook-Pro.local>
2018-08-15 21:02:42 -0600

    finished

--- src/App.js
+++ src/App.js
@@ -10,22 +10,29 @@ class App extends Component {
   }
 
   render() {
+    const { data, fetching } = this.props.wireCutter;
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to the test</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        { fetching &&
+          <p className="App-intro">Loading...</p>
+        }
+        { data && data.length > 0 &&
+          data.map(item => {
+            return <p className="App-intro" key={item.id} dangerouslySetInnerHTML={{__html: item.content.rendered}} />;
+          })
+        }
       </div>
     );
   }
 }
 
 const mapStateToProps = state => ({
- ...state
+ wireCutter: state.reducer303,
 })
 
 const mapDispatchToProps = dispatch => ({
